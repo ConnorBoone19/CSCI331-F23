76dfacc059a735d18e6646f16494cb8bd645cbf3
Connor Boone <jcboone@me.com>
2023-11-03 16:17:41 -0600

    HW8.3
    Added storefront

--- HW8/pages/storefront.js
+++ HW8/pages/storefront.js
@@ -1,22 +1,45 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 const MyStoreFront = () =>{
 
-    fetch('https://fakestoreapi.com/products')
-    .then(res=>res.json())
-    .then(json=>console.log(json[0].title))
-    
   
-  
-  
-    return (
-        <div>
-            <h1>HELLO</h1>
-            <label for="inputSearch">SEARCH</label><br></br>
-            <input id="inputSearch" type="search"/><br></br>
-        </div>
+    const [filteredProducts, setFilteredProducts] = useState([])
+    const [searchStr, setSearchStr] = useState("")
 
+    useEffect(fetchAPIdata, [searchStr])
+
+    function fetchAPIdata(){
+
+        fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(products=>{
+            const filtered = products.filter((el)=>{return el.title.toLowerCase().includes(searchStr.toLowerCase())})
+            const currentFilter = filtered.map((product,index)=>{
+                return (
+                    <div className="productStyle" key={index}>
+                        <p style={{fontStyle:".5rem"}}>{product.title}</p>
+                        <p>${product.price}</p>
+                        <img style={{width:"100px"}} src={product.image} alt={product.title}/>
+
+                    </div>
+                    );
+                }
+            )
+            setFilteredProducts(currentFilter)
+        }) 
+    }
+    useEffect(fetchAPIdata,[])
+
+    function handleChange(event){
+        setSearchStr(event.target.value)
+    }
+    return(
+        <div>
+            <p>SEARCH BAR</p>
+            <input type="text" value={searchStr} onChange={handleChange}/>
+            <div style={{display:"flex", flexFlow:"row-wrap"}}>{filteredProducts}</div>
+        </div>
     );
+
 }
 
 export default MyStoreFront
